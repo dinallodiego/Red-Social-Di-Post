fd04b0026c8d0cb1cc74f6861f786213429bb03e
Diego <diego.dinallo10@gmail.com>
2025-06-02 22:09:49 -0300

    estilos de login y registro, comienzo de back

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
+import { LoginComponent } from './login/login.component';
+import { MiPerfilComponent } from './mi-perfil/mi-perfil.component';
+import { PublicacionesComponent } from './publicaciones/publicaciones.component';
+import { RegistroComponent } from './registro/registro.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+  
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'mi-perfil', component: MiPerfilComponent },
+  { path: 'publicaciones', component: PublicacionesComponent },
+ 
+];
