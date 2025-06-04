4b5271f82af65bc9e1e1ef854c861740ab8b9ba3
Diego <diego.dinallo10@gmail.com>
2025-06-04 19:10:49 -0300

    registro y login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -8,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+
+  constructor(){}
+  usuarioIniciado:boolean=false;
+  mensaje: string = '';
+  colorMensaje: string = 'red'; 
+  //tomo los valores y los valido
+
+  async login() {
+    const email = (document.getElementById('email') as HTMLInputElement).value;
+    const password = (document.getElementById('password') as HTMLInputElement).value;
+
+    const contieneMayusculaYNumero = (cadena: string): boolean => {
+      const tieneMayuscula = /[A-Z]/.test(cadena);
+      const tieneNumero = /\d/.test(cadena);
+      return tieneMayuscula && tieneNumero;
+    };
+
+    if (password.length < 8) {
+      this.colorMensaje = 'red';
+      this.mensaje = 'La contraseña debe tener al menos 8 caracteres';
+    } else if (!contieneMayusculaYNumero(password)) {
+      this.colorMensaje = 'red';
+      this.mensaje = 'La contraseña debe tener al menos una mayúscula y un número';
+    } else {
+      this.colorMensaje = 'green';
+      this.mensaje = 'Ingreso correcto';
+      this.usuarioIniciado = true;
+    }
+
+    if(this.usuarioIniciado===true){
+      console.log('El email es:', email, 'y la contraseña es:', password);
+    }
+  }
+
+
+  //LOGICA CON LA BASE DE DATOS PARA INICIAR SESION 
+  
 }
